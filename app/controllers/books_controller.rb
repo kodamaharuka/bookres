75c939941c472fd4e731af2b2eb9694aa42d5ed5
kodamaharuka <mkhrauau@gmail.com>
2021-03-12 09:36:06 +0000

    until destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+   @books = Book.all
    @book = Book.new
   end
 
@@ -28,6 +29,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+   end
+
+   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
    end
 
   private
