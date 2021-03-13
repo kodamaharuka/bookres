911504d111f5c436c53f2d4397f06e718931902b
kodamaharuka <mkhrauau@gmail.com>
2021-03-13 19:10:07 +0000

    final fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,14 +8,12 @@ class BooksController < ApplicationController
    @book = Book.find(params[:id])
   end
 
-  #def new
-   #@book = Book.new
-  #end
 
   def create
      @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book),notice: 'Book was successfully updated.'
+    flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render "index"
@@ -26,15 +24,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    #book = Book.new(book_params)
-    #book.save
-    #redirect_to book_path(book)
   end
 
    def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end
    end
 
    def destroy
