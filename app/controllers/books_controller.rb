fc1e0b1fc979201fd3fa5d4603c773db8e555333
kodamaharuka <mkhrauau@gmail.com>
2021-03-13 10:22:43 +0000

    validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,13 @@ class BooksController < ApplicationController
   #end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(book)
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def edit
@@ -40,5 +44,5 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :body)
-  end
+   end
 end
